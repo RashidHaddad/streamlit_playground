f9a49a14283c1bb1a19959b6a54e9dad3d04f88e
Rashid Haddad <51336668+RashidHaddad@users.noreply.github.com>
2023-11-18 16:26:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,49 +7,103 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-import pypdf
+import bs4
+from langchain import hub
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.utilities import ApifyWrapper
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.memory import ConversationSummaryMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
+from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import DuckDuckGoSearchRun
 
 
-def generate_response_pdf(uploaded_file, openai_api_key, query_text):
-    # Load document if file is uploaded
-    if uploaded_file is not None:
-        documents = []
-        pdf = pypdf.PdfReader(uploaded_file)
-        for p in range(len(pdf.pages)):
-            page = pdf.pages[p]
-            text = page.extract_text()
-            documents += [text]
+os.environ["APIFY_API_TOKEN"] = "apify_api_SAP0d1xFEFs7mylGNKajb3WIcXSxYg3PDKLA"
+# os.environ["OPENAI_API_KEY"] = "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"
 
-    # documents = loader.load()
-    # Split documents into chunks
-    text_splitter=CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.create_documents(documents)
-    # Select embeddings
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    # Create a vectorstore from documents
-    db = Chroma.from_documents(texts, embeddings)
-    # Create retriever interface
-    retriever = db.as_retriever()
-    # Create QA chain
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-    return qa.run(query_text)
+
+apify = ApifyWrapper()
+# Call the Actor to obtain text from the crawled webpages
+loader = apify.call_actor(
+    actor_id="apify/website-content-crawler",
+    run_input={
+        "startUrls": [{"url": "https://www.cit.tum.de/en/cit/studies/students/"}]
+    },
+    dataset_mapping_function=lambda item: Document(
+        page_content=item["text"] or "", metadata={"source": item["url"]}
+    ),
+)
+
+data = loader.load()
+
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=HuggingFaceEmbeddings())
+
+llm = ChatOpenAI()
+memory = ConversationSummaryMemory(
+    llm=llm, memory_key="chat_history", return_messages=True
+)
+
+retriever = vectorstore.as_retriever()
+qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
+
+
+
+# def generate_response_pdf(uploaded_file, openai_api_key, query_text):
+#     # Load document if file is uploaded
+#     if uploaded_file is not None:
+#         documents = []
+#         pdf = pypdf.PdfReader(uploaded_file)
+#         for p in range(len(pdf.pages)):
+#             page = pdf.pages[p]
+#             text = page.extract_text()
+#             documents += [text]
+
+#     # documents = loader.load()
+#     # Split documents into chunks
+#     text_splitter=CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+#     texts = text_splitter.create_documents(documents)
+#     # Select embeddings
+#     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+#     # Create a vectorstore from documents
+#     db = Chroma.from_documents(texts, embeddings)
+#     # Create retriever interface
+#     retriever = db.as_retriever()
+#     # Create QA chain
+#     qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+#     return qa.run(query_text)
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Ask the Doc App')
 st.title('🦜🔗 Ask the Doc App')
-# File upload
-uploaded_file = st.file_uploader('Upload an article', type='pdf')
 # Query text
-query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
+query_text = st.text_input('Enter your question:', placeholder = 'How many credits do I need to graduate?')
 # Form input and query
 result = []
 with st.form('myform', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+    openai_api_key = "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"
+    submitted = st.form_submit_button('Submit', disabled=not(query_text))
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
-            response = generate_response_pdf(uploaded_file, openai_api_key, query_text)
+            response = qa(query_text)
             result.append(response)
-            del openai_api_key
+            # del openai_api_key
 if len(result):
     st.info(response)
